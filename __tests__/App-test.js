4d996c3bfb061bd458709403b3c0b5ec298cab2c
Michael Rozenblat <michael.rozenblat@pendo.io>
2022-10-06 14:50:36 -0400

    Jest Config

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,19 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+//import {PendoSDK, NavigationLibraryType} from 'rn-pendo-sdk';
+import {create, act} from 'react-test-renderer';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {idObj as PendoSDK} from 'identity-obj-proxy';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+
+it('renders correctly', async () => {
+  let tree
+  act(()=>{
+     tree = renderer.create(<App />);
+  })
+  
+  expect(tree.toJSON()).toMatchSnapshot();
 });
