8bc807857bd26617e8835fe9ae020714c82b39b1
Udi Levin <udi@pendo.io>
2023-06-12 18:03:33 +0300

    APP-80331 add support for Theme Provider and DataDog APP-80331 code cleanup APP-80331 readme update to include new integration APP-80331 add WithPendoModal

--- App.js
+++ App.js
@@ -3,39 +3,41 @@ import Login from './components/Login';
 import Plan from './components/Plan';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {PendoSDK, NavigationLibraryType, WithPendoReactNavigation} from 'rn-pendo-sdk';
+import {PendoSDK, NavigationLibraryType, WithPendoReactNavigation} from 'pendo';
+import {themes} from "./components/ThemeContext";
+import {ThemeProvider} from "./components/ThemeContext";
 
-function initPendo (){
-  const navigationOptions = {library: NavigationLibraryType.ReactNavigation};
-  const pendoKey = 'YOUR-APP-KEY';
-  PendoSDK.setup(pendoKey, navigationOptions);
-
+function initPendo() {
+    PendoSDK.setDebugMode(true);
+    const navigationOptions = {library: NavigationLibraryType.ReactNavigation};
+    const pendoKey = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IiIsInR5cCI6IkpXVCJ9.eyJkYXRhY2VudGVyIjoidXMiLCJrZXkiOiJmZWRiN2YyMDY1Y2FmYWFjZGFlZWNhMDNmOGRkNzgyNzRiNWEwNzMzZmIyMWUzNTgwOGViY2Y0YThiNTg3MjVjNmNkNzdmYzYyMDMyNmIyYjg0ZWU2YjNiNmZhY2I5OGNkZWRmMmJlOTRlMzFlNGNlNjNjYTE4NWZkNTIyYzJmM2E0OWIwMzA3YzkwZTA0OTgxY2Q1NDQ4ZWU5ZGIzYjA1YTg2OTQzYzgwYzBjZDA0YTAwNzU2MjJlMjRiYjFmMDViOGU3MzlhY2QwM2U3NmIwZjUxMGExMzBjNzI4NTVhNi5hYmMwYzk4OWVkMGFjOTAxNzNiNTMwYmEzOGIxZWJhNy41MzU1NzQwOWQ2ZjdlMTM4MjczNDkxZDc2NjhjYTNmNzJmZmJkYjU5OGMwYWJiMDk0ZTFlNDg0YzVlMWIwNGM3In0.em6YOaXKpEDAwmOCdf4CTIRDpyKbs9NNkVIZCDN6UwCkXgwtToaLO2PUoEmG0FG7te8d-i1qoowIKidCecuIb0hS9fooKHhyV36RdNGF-Icr4zs1Iw9jU7_asgZybajs7FKl7klJjmTzER8u3wRPVxiLopJG0SWCym_qw1KTaQ4';
+    PendoSDK.setup(pendoKey, navigationOptions);
 }
-initPendo();
 
+initPendo();
 const Stack = createNativeStackNavigator();
 
-function App(props) {
-  const PendoNavigationContainer = WithPendoReactNavigation(NavigationContainer);
-  return (
-    <PendoNavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-        name='Plan'
-        component={Plan}/>
-      </Stack.Navigator>
-    </PendoNavigationContainer>
-  );
+function App() {
+    const PendoNavigationContainer = WithPendoReactNavigation(NavigationContainer);
+    return (
+        <ThemeProvider>
+            <PendoNavigationContainer theme={themes.dark}>
+                <Stack.Navigator
+                    initialRouteName="Login"
+                    screenOptions={{
+                        headerShown: false,
+                    }}>
+                    <Stack.Screen
+                        name="Login"
+                        component={Login}
+                    />
+                    <Stack.Screen
+                        name='Plan'
+                        component={Plan}/>
+                </Stack.Navigator>
+            </PendoNavigationContainer>
+        </ThemeProvider>
+    );
 }
 
 export default App;
-
-
--- components/Login.js
+++ components/Login.js
@@ -1,95 +1,92 @@
-import { 
+import {
     View,
     Text,
     StyleSheet,
     SafeAreaView,
     TextInput,
     Button,
-  } from 'react-native'
-  import React from 'react'
-  import { PendoSDK } from 'rn-pendo-sdk';
+} from 'react-native'
+import React, {useContext} from 'react'
+import {PendoSDK} from 'pendo';
 
-  export default function Login({ route, navigation }) {
-  
+export default function Login({route, navigation}) {
+
     const [username, setUsername] = React.useState("");
     const [password, setPassword] = React.useState("");
     const [isLoggedIn, setLoggedin] = React.useState(true);
 
     function handleLogin() {
-      // ...
-      // Your authentication logic here
-      // ...
-      // For demo purposes, we will skip this part
-      // ...
+        // ...
+        // Your authentication logic here
+        // ...
+        // For demo purposes, we will skip this part
+        // ...
 
 
-      if (isLoggedIn){
-        // Your visitor is identified -> initialize Pendo
-        const visitorId = 'VISITOR-UNIQUE-ID'; 
-        const accountId = 'ACCOUNT-UNIQUE-ID';
-        const visitorData = {'Age': '25', 'Country': 'USA'}; // example data
-        const accountData = {'Tier': '1', 'Size': 'Enterprise'}; // example data
-    
-        PendoSDK.startSession(visitorId, accountId, visitorData, accountData);
+        if (isLoggedIn) {
+            // Your visitor is identified -> initialize Pendo
+            const visitorId = 'VISITOR-UNIQUE-ID';
+            const accountId = 'ACCOUNT-UNIQUE-ID';
+            const visitorData = {'Age': '25', 'Country': 'USA'}; // example data
+            const accountData = {'Tier': '1', 'Size': 'Enterprise'}; // example data
 
-        navigation.navigate('Plan')
-      }
+            PendoSDK.startSession(visitorId, accountId, visitorData, accountData);
+
+            navigation.navigate('Plan')
+        }
     }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Login</Text>
-        <SafeAreaView style={styles.form}>
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setUsername(text)}
-            value={username}
-            placeholder="Username"
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setPassword(text)}
-            value={password}
-            placeholder="Password"
-            secureTextEntry={true}
-          />
-          <View style={styles.buttonWrapper}>
-          <Button
-            onPress={handleLogin}
-            title="login"
-            color="#ec2059"
-          />
-          </View>
-        </SafeAreaView>
-        
-      </View>
+        <View>
+            <Text style={styles.title}>Login</Text>
+            <SafeAreaView style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={text => setUsername(text)}
+                    value={username}
+                    placeholder="Username"
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={text => setPassword(text)}
+                    value={password}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                />
+                <View style={styles.buttonWrapper}>
+                    <Button
+                        onPress={handleLogin}
+                        title="login"
+                        color="#ec2059"
+                    />
+                </View>
+            </SafeAreaView>
+
+        </View>
     )
-  }
-  const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#2f3641',
-      },
-      title: {
+}
+const styles = StyleSheet.create({
+    title: {
         color: '#ec2059',
         fontSize: 25,
         fontWeight: 'bold',
         padding: 40,
         alignSelf: 'center'
-      },
-      form: {
+    },
+    form: {
         padding: 20,
-      },
-      input: {
+    },
+    input: {
         height: 40,
         margin: 12,
         borderWidth: 1,
         borderRadius: 5,
         backgroundColor: '#fff',
         padding: 10,
-      },
-      buttonWrapper: {
+    },
+    buttonWrapper: {
         paddingTop: 25,
         paddingHorizontal: 135
-        
-      }
-  })
+
+    }
+})
